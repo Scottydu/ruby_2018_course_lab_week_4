322740f1f60dc237dd777500d3b078c669fdb4c5
mcamara <mcamara88@gmail.com>
2018-09-19 17:59:24 +0700

    Added me tests

--- spec/controllers/api/v1/me_controller_spec.rb
+++ spec/controllers/api/v1/me_controller_spec.rb
@@ -1,12 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MeController, type: :controller do
-
   describe "GET #index" do
-    it "returns http success" do
+    subject do
+      request.headers.merge(headers)
       get :index
-      expect(response).to have_http_status(:success)
+    end
+    let(:headers) { { } }
+
+    describe 'logged in user' do
+      let(:user) { create(:user) }
+      let(:auth_token) { JsonWebToken.encode(user_id: user.id) }
+      let(:headers) { {
+        'Authorization' => auth_token
+       } }
+
+      it { is_expected.to have_http_status :ok }
+
+      it 'is expected to have the user information' do
+        subject
+        expect(json_response['email']).to eq(user.email)
+        expect(json_response['name']).to eq(user.name)
+      end
+    end
+
+    describe 'unlogged user' do
+      it { is_expected.to have_http_status :unauthorized }
+
+      it 'returns errors' do
+        subject
+        expect(json_response.dig('errors', 'user')).to include('Invalid authentication')
+      end
     end
   end
-
 end
